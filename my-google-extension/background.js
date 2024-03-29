6caccde1f6f494beabedf904ef749a2701ee07f8
laraaskovic <lara.askovic@gmail.com>
2024-03-29 19:32:33 -0400

    commit

---
+++ my-google-extension/background.js
@@ -0,0 +1,20 @@
+chrome.commands.onCommand.addListener(function(command) {
+    if (command == "open-first-link") {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.scripting.executeScript({
+          target: {tabId: tabs[0].id},
+          function: openFirstLink
+        });
+      });
+    }
+  });
+  
+  function openFirstLink() {
+    let firstLink = document.querySelector('a[href^="http"]:not([href*="google.com"])');
+    if (firstLink) {
+      window.location.href = firstLink.href;
+    } else {
+      console.error("No link found.");
+    }
+  }
+  
