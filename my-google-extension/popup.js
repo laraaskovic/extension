a8292eae6c4070ec65a053430a779a242c1867fd
laraaskovic <lara.askovic@gmail.com>
2024-05-02 08:59:41 -0400

    s

--- my-google-extension/popup.js
+++ my-google-extension/popup.js
@@ -1,54 +1,7 @@
-
-/*const CLIENT_URL = "http://localhost:3010";
-
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.querySelector("#goPremium");
-    button.addEventListener("click", function() {
-        // Open a new tab with the checkout session URL
-        chrome.tabs.create({ url: "http://localhost:3010" });
-    });
-});
-*/
-
-/*const CLIENT_URL = "http://localhost:3010"; // Update with your client URL
-
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.querySelector("#goPremium");
-    button.addEventListener("click", function() {
-        // Open a new tab with the checkout session URL
-        chrome.tabs.create({ url: "about:blank" }, function(tab) {
-            chrome.tabs.executeScript(tab.id, {
-                code: `window.location.href = "${CLIENT_URL}/create-checkout-session";`
-            });
-        });
-    });
-});*/
-
-
-/*document.addEventListener("DOMContentLoaded", function() {
-    const button = document.querySelector("#goPremium")
-        button.addEventListener("click", function() {
-
-        fetch("http://localhost:3010/create-checkout-session", {
-            method: "POST"
-        })
-            .then(res => res.json())
-            .then(({ url }) => {
-            window.location = url
-            })
-            .catch(e => {
-            console.error(e.error)
-            })
-        })
-    
-  */  
-
-
-
     const goPremiumButton = document.getElementById("goPremium");
   
     goPremiumButton.addEventListener("click", function() {
-        chrome.tabs.create({ url: "landing.html" }, function(tab) {
+        chrome.tabs.create({ url: "http://localhost:3000/landing.html" }, function(tab) {
             chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
                 if (tabId === tab.id && changeInfo.status === "complete") {
                     chrome.scripting.executeScript({
