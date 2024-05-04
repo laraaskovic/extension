24b8a559e6b68c6a3d9fdca33e202b68fe950616
laraaskovic <lara.askovic@gmail.com>
2024-05-04 13:24:25 -0400

    s

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("client"));
 
 const stripe = require("stripe")(STRIPE_PRIVATE_KEY)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
